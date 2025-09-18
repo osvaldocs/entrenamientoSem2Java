80b5c6e3000e026d41780d95b154e690085c3d82
osvaldocs <pablo_campos92@hotmail.com.ar>
2025-09-17 19:51:54 -0500

    task1 donde

---
+++ src/model/Appliance.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Appliance extends Product{
+
+    public Appliance(double price, String name) {
+        super(price, name);
+    }
+
+    @Override
+    void getDescription() {
+
+    }
+}
---
+++ src/model/Food.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Food extends Product {
+    
+    public Food(double price, String name) {
+        super(price, name);
+    }
+
+    @Override
+    void getDescription() {
+
+    }
+}
